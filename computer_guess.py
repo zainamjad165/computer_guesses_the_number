dc9f9039ad4e40ea5a6487560d40f84fd031ad96
zainamjad165 <98817942+zainamjad165@users.noreply.github.com>
2022-02-21 10:39:07 +0500

    Update computer_guess.py

--- computer_guess.py
+++ computer_guess.py
@@ -1,21 +1,21 @@
 import random
 
 def computer_guess(x):
-    low=1
-    high=x
-    guess=0
-    feed_back= ""
+    lowest = 1
+    highest = x
+    guess = 0
+    feed_back = ""
     while feed_back != "c":
-        low == guess + 1
-        high == guess - 1
-        guess=int(random.randint(low,high))
-        feed_back=input(f"is {guess} too high (H), too low (L) or is corect (C)").lower()
+        lowest == guess + 1
+        highest == guess - 1
+        guess = int(random.randint(lowest,highest))
+        feed_back = input(f"Is {guess} too high (H), too low (L) or is corect (C)").lower()
         if feed_back == "h":
-            high=guess-1
+            highest = guess-1
             print(f"{guess} is too high")
         elif feed_back == "l":
-            low=guess+1
+            lowest = guess+1
             print(f"{guess} is too low")
-    print("you have guees the number corect!")            
+    print("Your guess is correct.")            
 
 computer_guess(1000)
